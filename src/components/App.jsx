8eb02bf1f21fd299fc7e4fb4489e74cdf8277d2d
Brewer <12806kight@gmail.com>
2023-10-26 00:30:11 -0500

    This is the start of a basic tictactoe application.

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function App() {
+  
+  function Square(){
+    return < button classname="square">X</button>
+  }
+  
+  return (
+    
+      < button classname="square">X</button>
+       );
+}
+
+export default App;
